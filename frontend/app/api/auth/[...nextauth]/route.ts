b990e4ba58b7a121ac25425784bf5dde18e74109
holyaustin <holyaustin@yahoo.com>
2023-09-22 20:58:19 +0100

    client commit

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,10 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/lib/prisma";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import prisma from "@/lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  //adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
--- frontend/lib/prisma.ts
+++ frontend/lib/prisma.ts
@@ -1,3 +1,4 @@
+export { };
 /**
 import { PrismaClient } from "@prisma/client";
 
